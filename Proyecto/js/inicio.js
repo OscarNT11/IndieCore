1ce96e10ce102842ac53dfaff5bb3f691c64ed01
Agustin-Calvette <whitex6d@gmail.com>
2026-09-01 16:20:52 -0300

    Login, registro y validación
    Termino de imlementar el login, registro y la validación de los formularios.

--- Proyecto/js/inicio.js
+++ Proyecto/js/inicio.js
@@ -1,10 +1,82 @@
 const botonIngresar = document.querySelector(".btn-ingresar");
 
-const botonRegistro = document.getElementById("crear-cuenta");
+const botonIngreso = document.getElementById("volver-login");
 
 const pantallaTransparente = document.getElementById("pantalla-transparente");
 
-const cerrarLogin = document.getElementById("cerrar-login");
+const cerrarVentana = document.querySelectorAll(".cerrar-ventana");
+
+const botonRegistro = document.getElementById("crear-cuenta");
+
+const pantallaRegistro = document.getElementById("pantalla-registro");
+
+const formularioRegistro = document.getElementById("register");
+
+const formularioLogin = document.getElementById("login");
+
+const limite = 20;
+
+formularioLogin.addEventListener("submit", (evento) => {
+
+    evento.preventDefault();
+
+    const contraseña = document.getElementById("ingresar-cont-login").value;
+
+    if (contraseña.length > limite) {
+        alert("La contraseña no puede superar los 20 caracteres");
+    }
+
+    const nombre = document.getElementById('ingresar-usua-login').value;
+
+    if (nombre.length > limite) {
+        alert("El nombre de usuario no puede superar los 20 caracteres");
+    }
+
+
+});
+
+formularioRegistro.addEventListener("submit", (evento) => {
+
+    evento.preventDefault();
+
+    const contraseña = document.getElementById("ingresar-cont-registro").value;
+    const confirmarContraseña = document.getElementById("ingresar-conf-registro").value;
+
+    if (contraseña !== confirmarContraseña) {
+        alert("Las contraseñas no coinciden");
+    return;
+    }
+
+    if (contraseña.length > limite) {
+        alert("La contraseña no puede superar los 20 caracteres");
+    }
+
+    const nombre = document.getElementById('ingresar-usua-registro').value;
+
+    if (nombre.length > limite) {
+        alert("El nombre de usuario no puede superar los 20 caracteres");
+    }
+
+
+});
+
+botonRegistro.addEventListener("click", (evento) => {
+
+    evento.preventDefault();
+
+    pantallaRegistro.style.display = "flex";
+
+});
+
+botonIngreso.addEventListener("click", (evento) => {
+
+    evento.preventDefault();
+
+    pantallaRegistro.style.display = "none";
+
+    pantallaTransparente.style.display = "flex";
+
+});
 
 botonIngresar.addEventListener("click", () => {
 
@@ -12,14 +84,13 @@ botonIngresar.addEventListener("click", () => {
 
 });
 
-cerrarLogin.addEventListener("click", () => {
+cerrarVentana.forEach((boton) => {
 
-    pantallaTransparente.style.display = "none";
+    boton.addEventListener("click", () => {
 
-});
+        pantallaTransparente.style.display = "none";
+        pantallaRegistro.style.display = "none";
 
-botonRegistro.addEventListener("click", () => {
-
-    pantallaTransparente.style.display = "flex";
+    });
 
 });
